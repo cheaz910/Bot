7c73f9b558fb2ef86aedb14f930e01892f04e387
cheaz910 <cheaz910@mail.ru>
2018-09-24 22:59:59 +0500

    Added 2 commands

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,8 +1,16 @@
 import java.io.*;
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
-import com.github.cliftonlabs.json_simple.JsonObject;
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
 
 public class Bot {
@@ -10,21 +18,88 @@ public class Bot {
 
     public void Start() throws FileNotFoundException {
         NameOfUser = GetName();
-        Boolean t = CheckNameInFile(NameOfUser);
-        System.out.println(t);
-        JsonObject jObject = new JsonObject();
+
+
+        DateFormat ff = new SimpleDateFormat("dd-MM-yy");
+        Date currentDate = new Date(2018,12,2,16,9,30);
+        final String dateString = ff.format(currentDate);
+        Map<String, Map<String, ArrayList<String>>> log = new HashMap<>(); // тут нужно получить Map из файла
+        log.put("Ilnur", new HashMap<String, ArrayList<String>>());
+        log.get("Ilnur").put(dateString, new ArrayList<String>() {{
+            add("CompleteTask");
+        }});
+
+        String testim = ConvertToJson(log);
+        System.out.println(testim);
+        System.out.println(ConvertToMap(testim));
+
+
+        if (!log.containsKey(NameOfUser)) {
+            log.put(NameOfUser, new HashMap());
+        }
+
+
+        /**
+         * тут нужно загрузить словарь из файла(json) и поместить в переменную log
+         * изменить метод CheckNameInFile полностью и проверять имя как ключ в log, иначе добавить
+         *
+         */
+
+
+
+        System.out.println(dateString);
+        try {
+            System.out.println(ff.parse(dateString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(log);
+        ConvertToJson(log);
+        // JsonObject jObject = new JsonObject();
         System.out.println(GetGreeting(NameOfUser));
         String command = "";
         Scanner in = new Scanner(System.in);
-        while (!"bye".equals(command)) {
+        while (!"Пока".equals(command)) {
             command = in.nextLine();
-            System.out.println(command);
+            System.out.println("Command:" + command);
+            if (command.startsWith("Добавь заметку ")) {
+                AddNote(command.substring(15), log);
+            }
+            else if (command.startsWith("Покажи список дел ")) {
+                String result = GetNotesForDay(command.substring(18), log);
+                System.out.println(result);
+            }
         }
+        // Здесь нужно сохранять словарь в файл
+    }
+
+    private String GetNotesForDay(String command, Map<String, Map<String, ArrayList<String>>> log) {
+        return log.get(NameOfUser).get(command).toString();
+    }
+
+    private void AddNote(String command, Map<String, Map<String, ArrayList<String>>> log) {
+        final String[] info = command.split(" ");
+        Map<String, ArrayList<String>> notes = log.get(NameOfUser);
+        if (notes.containsKey(info[0]))
+            notes.get(info[0]).add(info[1]);
+        else
+            notes.put(info[0], new ArrayList() {{add(info[1]);}});
+    }
+
+    private String ConvertToJson(Map<String, Map<String, ArrayList<String>>> log) {
+        String jsonStr = new Gson().toJson(log, HashMap.class);
+        return jsonStr;
+    }
+
+    private Map<String, Map<String, ArrayList<String>>> ConvertToMap(String log) {
+        Map<String, Map<String, ArrayList<String>>> myMap = new Gson().fromJson(log, HashMap.class);
+        return myMap;
     }
 
 
-    public Boolean CheckNameInFile(String name) throws FileNotFoundException {
-        File file = new File("Names.txt");
+    public Boolean CheckNameInFile(String name, Map<String, Map<String, String>> log) throws FileNotFoundException {
+        /** Работающее считывание файла
+         * File file = new File("Names.txt");
         Scanner scanner = new Scanner(file);
         List<String> list=new ArrayList<>();
         while(scanner.hasNextLine()){
@@ -33,7 +108,8 @@ public class Bot {
         }
         System.out.println(list);
         scanner.close();
-        return list.contains(name);
+        return list.contains(name);*/
+        return true;
     }
 
     public void AddNameToFile(String name) {
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
  * Asks user for a name, then greets the user.
  */
 public class Program {
-    public static void main(String[] args ) throws FileNotFoundException {
+    public static void main(String[] argv) throws FileNotFoundException {
         Bot bot = new Bot();
         bot.Start();
     }
