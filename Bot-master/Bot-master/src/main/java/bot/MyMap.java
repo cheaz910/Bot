169c7fafaccad807c86dbcd83a189d29cacfb458
cheaz910 <cheaz910@mail.ru>
2018-10-03 03:06:24 +0500

    Удаление лишней папки?

--- Bot-master/Bot-master/src/main/java/bot/MyMap.java
+++
@@ -1,8 +0,0 @@
-package bot;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class MyMap extends HashMap<String, Map<String, Log>> {
-
-}
--- Bot-master/src/main/java/bot/Program.java
+++
@@ -1,8 +0,0 @@
-package bot;
-
-public class Program {
-    public static void main(String[] argv){
-        Bot bot = new Bot();
-        bot.Start();
-    }
-}
