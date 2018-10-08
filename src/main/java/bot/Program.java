3647990949781dbb865e7ebf210871cea005bde9
cheaz910 <cheaz910@mail.ru>
2018-10-08 19:27:55 +0500

    Добавлен конструктор к боту, принимающий PrintStream и InputStream и переделаны тесты

--- src/main/java/bot/Program.java
+++ src/main/java/bot/Program.java
@@ -2,7 +2,7 @@ package bot;
 
 public class Program {
     public static void main(String[] argv){
-        Bot bot = new Bot();
+        Bot bot = new Bot(System.out, System.in);
         bot.Start();
     }
 }
